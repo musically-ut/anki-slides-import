2dfc7771c96e4c60e7a2fcb32aa02938adc58667
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-02-25 11:53:55 +0100

    Add pdf page parsing and tests.

---
+++ slidesimport/pdfpages.py
@@ -0,0 +1,22 @@
+from wand.image import Image
+import unittest
+
+class PdfPages:
+    def __init__(self, pdfFileName):
+        self.pdf = Image(filename=pdfFileName)
+
+    def getPageAsPng(self, pageNumber, width=640):
+        """Return the given page as a pdf wand.image.Image."""
+        # The page numbers are indexed with base 0, while page numbers start
+        # from 1.
+        img = Image(image = self.pdf.sequence[pageNumber - 1])
+        img.resize(width, long(width * img.height / (1.0 * img.width)))
+        return img.convert('png')
+
+
+class TestPdfPages(unittest.TestCase):
+    def testOnVegFoodInJapan(self):
+        p = PdfPages('./slidesimport/test/Veg-food-in-Japan.pdf')
+        slide12 = Image(filename='./slidesimport/test/slide-12.png')
+        self.assertEqual(p.getPageAsPng(12), slide12)
+
